3c78029e79862c3341cdd8aa0dc1efceadccd8e7
mamun-prodhan <mamunpr9@gmail.com>
2022-11-04 18:20:18 +0600

    mongodb database added

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('Simple Node Server Running');
+    res.send('Simple Node Seoorver Running');
 });
 
 app.use(cors());
@@ -16,6 +17,21 @@ const users = [
     { id: 3, name: 'Sabila', email: 'sabila@gmail.com' }
 ];
 
+//userName: dbUser1
+//password: Yp7ii9GZ7iEHXqFs
+
+
+const uri = "mongodb+srv://dbUser1:Yp7ii9GZ7iEHXqFs@cluster0.nhx4fnh.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log('database connected');
+  client.close();
+});
+
+
+
 app.get('/users', (req, res) =>{
     res.send(users);
 })
@@ -23,9 +39,10 @@ app.get('/users', (req, res) =>{
 app.post('/users', (req, res) =>{
     console.log('Post Api called');
     const user = req.body;
-    user.id = user.length + 1;
+    user.id = users.length + 1;
     users.push(user);
-    console.log(req.body);
+    console.log(user);
+    res.send(user);
 })
 
 app.listen(port, () => {
